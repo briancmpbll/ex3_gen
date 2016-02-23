2b4fc8f83da981a0bc62bd4b2e85922be969e527
briancmpbll <briancmpbll@gmail.com>
2016-02-22 19:59:00 -0800

    Adding initial integration testing for signup

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -3,5 +3,23 @@ FactoryGirl.define do
     sequence(:email) { |i| "email#{i}@email.com" }
     password 'abc123'
     password_confirmation 'abc123'
+
+    factory :blank_user do
+      email ''
+      password ''
+      password_confirmation ''
+    end
+
+    factory :user_blank_email do
+      email ''
+    end
+
+    factory :user_blank_password do
+      password ''
+    end
+
+    factory :user_blank_confirmation do
+      password_confirmation ''
+    end
   end
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,39 @@
+require 'test_helper'
+
+# Integration test for signing up a new user.
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  context 'submitting the signup form' do
+    setup do
+      get signup_path
+      @user_count = User.count
+    end
+
+    context 'with a blank user' do
+      setup do
+        post users_path, user: FactoryGirl.attributes_for(:blank_user)
+      end
+
+      should 'not create a user' do
+        assert_equal(@user_count, User.count)
+      end
+
+      should 'render the new user template' do
+        assert_template 'users/new'
+      end
+    end
+
+    context 'with a valid user' do
+      setup do
+        post_via_redirect users_path, user: FactoryGirl.attributes_for(:user)
+      end
+
+      should 'create a user' do
+        assert_equal(User.count - @user_count, 1)
+      end
+
+      should 'render the show template' do
+        assert_template 'users/show'
+      end
+    end
+  end
+end
