8f7cc73f92d3c9e1b071992a25da548c99582004
briancmpbll <briancmpbll@gmail.com>
2016-02-27 22:31:45 -0800

    Change gravatar_for to use an options hash

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,9 @@
 # Helper functions for the Users controller
 module UsersHelper
   # Returns a gravatar for the given user.
-  def gravatar_for(user, size = 80)
+  def gravatar_for(user, options = { size: 80 })
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{options[:size]}"
     image_tag(gravatar_url, alt: user.email, class: 'gravatar')
   end
 
